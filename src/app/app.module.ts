1abb2f692c32a413fb3f562371b1db8549a8e029
emilioenlaluna <al244182@edu.uaa.mx>
2023-07-13 20:03:58 -0500

    Componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ContactComponent } from './components/contact/contact.component';
+import { EducationComponent } from './components/education/education.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { ServicesComponent } from './components/services/services.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProjectsComponent
+    ProjectsComponent,
+    ContactComponent,
+    EducationComponent,
+    ExperienceComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
