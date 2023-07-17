b8c23fa667dadf15fcd6cf642a15cd3c47593c24
Emilio Luna <74982686+emilioenlaluna@users.noreply.github.com>
2023-07-17 08:05:12 +0000

    Firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebase: {
+        apiKey: "AIzaSyCxpNHxPt8PrjtVIc6YWHz9vdzzIDcHO7w",
+        authDomain: "emilioenlaluna.firebaseapp.com",
+        projectId: "emilioenlaluna",
+        storageBucket: "emilioenlaluna.appspot.com",
+        messagingSenderId: "71386887821",
+        appId: "1:71386887821:web:cee0f00d6d1873a5e0d4c4"
+    },
+      production: false,
+    };
