7a5621c4c098f39520417bb8405969589f84baac
Emilio Luna <74982686+emilioenlaluna@users.noreply.github.com>
2023-07-20 10:35:24 +0000

    Contact and cv

--- src/app/components/cv/cv.component.ts
+++ src/app/components/cv/cv.component.ts
@@ -8,4 +8,20 @@ import { Component } from '@angular/core';
 export class CvComponent {
   src = 'https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf';
   // pdfSrc: string | PDFSource | ArrayBuffer = './assets/pdf-test.pdf';
+  downloadPdf() {
+    const pdfUrl = 'https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf'; // Reemplaza con la URL del archivo PDF en GitHub o en otro servidor accesible
+    const link = document.createElement('a');
+    link.href = pdfUrl;
+    link.download = 'nombre_del_archivo.pdf'; // Cambia el nombre del archivo descargado si lo deseas
+    link.target = '_blank'; // Abre en una nueva pestaña, opcional
+    link.click();
+  }
+  downloadPdfSpanish() {
+    const pdfUrl = 'https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf'; // Reemplaza con la URL del archivo PDF en GitHub o en otro servidor accesible
+    const link = document.createElement('a');
+    link.href = pdfUrl;
+    link.download = 'nombre_del_archivo.pdf'; // Cambia el nombre del archivo descargado si lo deseas
+    link.target = '_blank'; // Abre en una nueva pestaña, opcional
+    link.click();
+  }
 }
